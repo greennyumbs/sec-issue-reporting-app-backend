89c38c0b9688a4d7c93695d30fb86ac823c6c5cf
Rujikorn Rujitanont <rujikornkin96@gmail.com>
2024-07-30 23:29:42 +0700

    delete

--- config/config.ts
+++
@@ -1,7 +0,0 @@
-export default () => ({
-  port: process.env.PORT,
-  supabase: {
-    url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_KEY,
-  },
-});
--- src/technician/technician.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TechnicianService } from './technician.service';
-import { TechnicianController } from './technician.controller';
-
-@Module({
-  controllers: [TechnicianController],
-  providers: [TechnicianService],
-})
-export class TechnicianModule {}
